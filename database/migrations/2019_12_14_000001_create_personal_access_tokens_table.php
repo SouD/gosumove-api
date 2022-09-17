345ca1f7e921e9ddad0f2ff44761c192b040d768
Linus Sörensen <linus.sorensen@yaytrade.com>
2022-09-17 13:15:31 +0200

    Fix issue in personal_access_token migration.

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -6,23 +6,31 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up(): void
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->uuid('id');
             $table->uuidMorphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)
-                ->unique();
-            $table->text('abilities')
-                ->nullable();
-            $table->timestamp('last_used_at')
-                ->nullable();
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
 
-    public function down(): void
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
     {
         Schema::dropIfExists('personal_access_tokens');
     }
